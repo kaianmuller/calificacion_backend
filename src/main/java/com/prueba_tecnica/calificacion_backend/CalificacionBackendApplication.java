bc5ccfb2a020ee4c8b76e1f485b2c237dacca37c
DESKTOP-67SCBI8\KYB <kaianmuller100@gmail.com>
2021-12-23 12:22:15 -0300

    Inicio del proyecto.

---
+++ src/main/java/com/prueba_tecnica/calificacion_backend/CalificacionBackendApplication.java
@@ -0,0 +1,13 @@
+package com.prueba_tecnica.calificacion_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalificacionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalificacionBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba_tecnica/calificacion_backend/CalificacionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba_tecnica.calificacion_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalificacionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
