d00059de92d392c28df475716bc0e7b9b7c68cef
kaianmuller <kaianmuller100@gmail.com>
2021-12-28 12:48:32 -0300

    Cambio en secret key.

--- src/main/java/com/prueba_tecnica/calificacion_backend/constants/Constants.java
+++ src/main/java/com/prueba_tecnica/calificacion_backend/constants/Constants.java
@@ -2,7 +2,7 @@ package com.prueba_tecnica.calificacion_backend.constants;
 
 public class Constants {
     
-    public static final String SECRET = "SECRET_KEY";
+    public static final String SECRET = "CALIFIC";
     public static final long EXPIRATION_TIME = 900_000; // 15 mins
 
 
