cd08b1ddd4f5d0752d8e8d2b17a646274839792a
kaianmuller <kaianmuller100@gmail.com>
2021-12-28 13:44:04 -0300

    Eliminacion de constants de git.

--- src/main/java/com/prueba_tecnica/calificacion_backend/constants/Constants.java
+++
@@ -1,9 +0,0 @@
-package com.prueba_tecnica.calificacion_backend.constants;
-
-public class Constants {
-    
-    public static final String SECRET = "CALIFIC";
-    public static final long EXPIRATION_TIME = 900_000; // 15 mins
-
-
-}
